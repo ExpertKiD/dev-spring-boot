0dc7499717b6d59b0bea02236e1c8eb8aef2b0ac
Suman Adhikari <expertxkid@gmail.com>
2023-07-26 21:15:44 +0545

    feat: complete 02-component-scanning

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/SpringCoreDemoApplication.java
@@ -0,0 +1,18 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// For component scanning other packages
+//@SpringBootApplication(
+//        scanBasePackages = {
+//                "com.luv2code.springcoredemo",
+//                "com.luv2code.util",
+//        })
+@SpringBootApplication
+public class SpringCoreDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreDemoApplication.class, args);
+    }
+
+}
